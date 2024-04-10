09985f88ae849f84cf43c3a61798afbe52b4b301
James <logicism81@gmail.com>
2024-04-10 01:36:19 -0400

    Added DB config

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 // import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const queryClient = postgres(`postgres://${process.env.POSTGRES_USERNAME}@127.0.0.1:5432/postgres`);
+const queryClient = postgres(`postgres://${process.env.PG_USERNAME}@${process.env.PG_URL}:${process.env.PG_PORT}/${process.env.PG_DB}`);
 const db = drizzle(queryClient);
 
 export default db;
