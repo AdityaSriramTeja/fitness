fc7f611e129c3d3919db321f569ab6400f007393
James <logicism81@gmail.com>
2024-04-10 01:30:41 -0400

    Added env file

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 // import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const queryClient = postgres("postgres://james@127.0.0.1:5432/postgres");
+const queryClient = postgres(`postgres://${process.env.POSTGRES_USERNAME}@127.0.0.1:5432/postgres`);
 const db = drizzle(queryClient);
 
 export default db;
