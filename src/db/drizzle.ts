3031b721634c69ccb126fef9f247a3fffd261e1c
James <logicism81@gmail.com>
2024-04-10 01:42:31 -0400

    Aded pw

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 // import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const queryClient = postgres(`postgres://${process.env.PG_USERNAME}@${process.env.PG_URL}:${process.env.PG_PORT}/${process.env.PG_DB}`);
+const queryClient = postgres(`postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_URL}:${process.env.PG_PORT}/${process.env.PG_DB}`);
 const db = drizzle(queryClient);
 
 export default db;
