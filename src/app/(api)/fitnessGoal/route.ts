b4a775b19edffa4ba5c7caa8a5f812b1862fff5a
Thomas.N <thomasnolasque@cmail.carleton.ca>
2024-04-12 18:42:36 -0400

    Fitness Get Works

--- src/app/(api)/fitnessGoal/route.ts
+++ src/app/(api)/fitnessGoal/route.ts
@@ -1,7 +1,18 @@
 import { getFitnessGoal } from "@/db/fitnessGoal";
+import { getFitnessGoalByMember } from "@/db/fitnessGoal";
 
 export async function GET(request: Request) {
   const data = await getFitnessGoal();
+  return new Response(JSON.stringify(data), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const data = await getFitnessGoalByMember(body.username);
   return new Response(JSON.stringify(data), {
     headers: {
       "content-type": "application/json",
