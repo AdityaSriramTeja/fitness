426140e7918edaf1b4065c7b5d0766c8c16518df
James <logicism81@gmail.com>
2024-04-12 03:44:09 -0400

    Finished Log In

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,9 +3,9 @@ import RedirectButton from "@/components/shared/RedirectBtn";
 export default function Home() {
   return (
     <div className="h-full w-full flex items-center ">
-      <RedirectButton route={"/member"}>Member Page</RedirectButton>
-      <RedirectButton route={"/admin"}>Admin Page</RedirectButton>
-      <RedirectButton route={"/trainer"}>Trainer Page</RedirectButton>
+      <RedirectButton route={"/login/member"}>Member Page</RedirectButton>
+      <RedirectButton route={"/login/admin"}>Admin Page</RedirectButton>
+      <RedirectButton route={"/login/trainer"}>Trainer Page</RedirectButton>
     </div>
   );
 }
