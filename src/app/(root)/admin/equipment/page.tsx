d6cccdbda0b60fe766088388c9ba61a993ef6b91
James <logicism81@gmail.com>
2024-04-12 22:24:25 -0400

    Added equipment page

---
+++ src/app/(root)/admin/equipment/page.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import React from "react";
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer, Checkbox } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import { ParsedEquipmentType } from "@/db/equipment";
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+export default function Equipment() {
+  async function fetchEquipment() {
+    const res = await fetch(`/api/equipment`);
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`Error fetching equipment`);
+    }
+
+    return data;
+  }
+
+  async function updateEquipmentMaintenance(id: number, needs_maintenance: boolean) {
+    const res = await fetch(`/api/equipment`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id, needs_maintenance }),
+    });
+
+    if (!res.ok) {
+      throw new Error(`Error updating equipment maintenance`);
+    }
+
+    refetch();
+  }
+
+  const { data, isLoading, refetch } = useQuery<ParsedEquipmentType[]>({
+    queryKey: ["getEquipment"],
+    queryFn: fetchEquipment,
+  });
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: { id: number; needs_maintenance: boolean }): Promise<void> => {
+      await updateEquipmentMaintenance(data.id, data.needs_maintenance);
+    },
+  });
+
+  return (
+    <div>
+      <Heading mb="6">Manage Equipment {isLoading && <Spinner />}</Heading>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>List of all gym equipment</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Room</Th>
+              <Th>Maintenance</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {data?.map((equipment) => (
+              <Tr key={equipment.id}>
+                <Td>{equipment.name}</Td>
+                <Td>{equipment.room_name}</Td>
+                <Td>
+                  <Checkbox disabled={isPending} isChecked={!equipment.needs_maintenance} onChange={() => mutate({ id: equipment.id, needs_maintenance: !equipment.needs_maintenance })} />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
--- src/db/equipment.ts
+++ src/db/equipment.ts
@@ -8,7 +8,33 @@ export type EquipmentType = {
   room_id: number;
 };
 
+export type ParsedEquipmentType = {
+  id: number;
+  name: string;
+  needs_maintenance: boolean;
+  room_name: string;
+};
+
 export async function getEquipment(): Promise<EquipmentType[]> {
   const equipment = await db.execute(sql`select * from Equipment`);
   return equipment as unknown as EquipmentType[];
+}
+
+export async function getEquipmentParsed(): Promise<ParsedEquipmentType[]> {
+  const equipment = await db.execute(sql`
+    select e.id, e.name, e.needs_maintenance, r.name as room_name
+    from Equipment e
+    left join Room r on e.room_id = r.id
+    order by e.id
+  `);
+  return equipment as unknown as ParsedEquipmentType[];
+}
+
+export async function updateEquipmentMaintenance(id: number, needs_maintenance: boolean): Promise<void> {
+  await db.execute(
+    sql`update Equipment
+    set needs_maintenance = ${needs_maintenance}
+    where id = ${id}
+    `
+  );
 }
