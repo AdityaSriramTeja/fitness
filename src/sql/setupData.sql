109a4ba209d7f9c258108e3cf00d75565e9c26bf
Thomas.N <thomasnolasque@cmail.carleton.ca>
2024-04-12 22:53:01 -0400

    New setupData.sql

--- src/sql/setupData.sql
+++ src/sql/setupData.sql
@@ -1,11 +1,11 @@
 INSERT INTO Admin (username, password, name) VALUES
-  ('admin1', 'admin1', 'MrAdmin1'),
-  ('admin2', 'admin2', 'MrAdmin2');
+  ('admin1', 'admin1', 'Quinlan'),
+  ('admin2', 'admin2', 'Chibuzo');
 
 INSERT INTO Trainer (username, password, name) VALUES
-  ('trainer1', 'trainer1', 'NinjaTrainer1'),
-  ('trainer2', 'trainer2', 'NinjaTrainer2'),
-  ('trainer3', 'trainer3', 'NinjaTrainer3');
+  ('trainer1', 'trainer1', 'Qiu'),
+  ('trainer2', 'trainer2', 'Carson'),
+  ('trainer3', 'trainer3', 'Laurie');
 
 INSERT INTO Trainer_Availability (day, starting_time, trainer_username) VALUES
   ('Monday', '09:00:00', 'trainer1'),
@@ -20,28 +20,27 @@ INSERT INTO Trainer_Availability (day, starting_time, trainer_username) VALUES
   ('Wednesday', '17:00:00', 'trainer3');
 
 INSERT INTO Room (name) VALUES
-  ('Room 1'),
-  ('Room 2'),
-  ('Room 3');
+  ('Algonquin Gym'),
+  ('Carleton Gym'),
+  ('Ottawa Gym');
 
 INSERT INTO Class (name, is_group_class, room_id, day, starting_time, trainer_username) VALUES
-  ('Class 1', TRUE, (SELECT id FROM Room WHERE name = 'Room 1'), 'Monday', '09:00:00', 'trainer1'),
-  ('Class 2', TRUE, (SELECT id FROM Room WHERE name = 'Room 2'), 'Monday', '12:00:00', 'trainer2'),
-  ('Class 3', TRUE, (SELECT id FROM Room WHERE name = 'Room 3'), 'Monday', '15:00:00', 'trainer3'),
-  ('Class 4', TRUE, (SELECT id FROM Room WHERE name = 'Room 1'), 'Monday', '10:00:00', 'trainer1'),
-  ('Class 5', FALSE, (SELECT id FROM Room WHERE name = 'Room 2'), 'Monday', '13:00:00', 'trainer2');
-
+  ('Zero To Hero', TRUE, (SELECT id FROM Room WHERE name = 'Algonquin Gym'), 'Monday', '09:00:00', 'trainer1'),
+  ('Hero To Zero', TRUE, (SELECT id FROM Room WHERE name = 'Carleton Gym'), 'Tuesday', '12:00:00', 'trainer2'),
+  ('Zero to Zero', TRUE, (SELECT id FROM Room WHERE name = 'Ottawa Gym'), 'Monday', '15:00:00', 'trainer3'),
+  ('Hero to Hero', FALSE, (SELECT id FROM Room WHERE name = 'Algonquin Gym'), 'Monday', '10:00:00', 'trainer1'),
+  ('Some Random Class', FALSE, (SELECT id FROM Room WHERE name = 'Carleton Gym'), 'Tuesday', '13:00:00', 'trainer2');
 
 INSERT INTO Member (username, password, name, outstanding_balance, enrolled_class_id) VALUES
-  ('member1', 'member1', 'Bob1', 0, 1),
-  ('member2', 'member2', 'Bob2', 0, 2),
-  ('member3', 'member3', 'Bob3', 0, 3),
-  ('member4', 'member4', 'Bob4', 0, 4),
-  ('member5', 'member5', 'Bob5', 0, 5);
+  ('member1', 'member1', 'Akuchi', 0, 1),
+  ('member2', 'member2', 'Onyx', 0, 2),
+  ('member3', 'member3', 'Cyan', 0, 3),
+  ('member4', 'member4', 'Olayinka', 0, 4),
+  ('member5', 'member5', 'Ora', 0, NULL);
 
 INSERT INTO Achievements_Log (name, description, date, username) VALUES
-  ('Achievement 1', 'Description 1', '2021-01-01', 'member1'),
-  ('Achievement 2', 'Description 2', '2021-01-01', 'member2');
+  ('Arms of Steel', 'Bench Pressed 225lb', '2021-02-03', 'member1'),
+  ('Legs of Iron', 'Did a 10k run', '2024-05-06', 'member2');
 
 INSERT INTO Health_Profile (weight, average_sleep, average_calories_burnt, gender, age, username) VALUES
   (110, 1, 1100, 'M', 21, 'member1'),
@@ -51,18 +50,17 @@ INSERT INTO Health_Profile (weight, average_sleep, average_calories_burnt, gende
   (150, 5, 1500, 'M', 25, 'member5');
 
 INSERT INTO Equipment (name, needs_maintenance, room_id) VALUES
-  ('Equipment 1.1', FALSE, (SELECT id FROM Room WHERE name = 'Room 1')),
-  ('Equipment 1.2', TRUE, (SELECT id FROM Room WHERE name = 'Room 2')),
-  ('Equipment 2', FALSE, (SELECT id FROM Room WHERE name = 'Room 3'));
+  ('Dumbbells', FALSE, (SELECT id FROM Room WHERE name = 'Algonquin Gym')),
+  ('Squat Rack', TRUE, (SELECT id FROM Room WHERE name = 'Carleton Gym')),
+  ('Cycling Machine', FALSE, (SELECT id FROM Room WHERE name = 'Ottawa Gym'));
 
 INSERT INTO Exercise_Routine_Log (name, date, num_reps, username) VALUES
-  ('Routine 1', '2021-01-01', 10, 'member1'),
-  ('Routine 2', '2021-01-01', 20, 'member2');
+  ('Benchpressing', '2021-02-03', 10, 'member1'),
+  ('Jumping In The Air', '2024-05-06', 20, 'member2');
 
 INSERT INTO Fitness_Goal (name, description, date, username) VALUES
-  ('Goal 1', 'Description 1', '2021-01-01', 'member1'),
-  ('Goal 2', 'Description 2', '2021-01-01', 'member2');
-
+  ('Have a 6-pack', 'This will require doing many sit-ups.', '2021-02-03', 'member1'),
+  ('Can jump more then 2.5 meters high', 'This will require lots of leg strength.', '2024-05-06', 'member2');
 
 INSERT INTO Transactions (name, username, amount, date) VALUES
-  ('Membership Free', 'member1', 20, '2021-01-01');
+  ('Membership Fee', 'member1', 20, '2021-02-03');
